e92d3a39558f87f664da7e3556b4cbe11a3ff7f8
William Chong <31718249+chongwilliam@users.noreply.github.com>
2024-04-16 16:34:45 -0700

    Update redis_keys.h

--- homework/hw1/redis_keys.h
+++ homework/hw1/redis_keys.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // - read:
-const std::string JOINT_ANGLES_KEY = "sai2::interfaces::robot_dq::PANDA";
-const std::string JOINT_VELOCITIES_KEY = "sai2::interfaces::robot_q::PANDA";
+const std::string JOINT_ANGLES_KEY = "sai2::interfaces::robot_q::PANDA";
+const std::string JOINT_VELOCITIES_KEY = "sai2::interfaces::robot_dq::PANDA";
 // - write
 const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::interfaces::robot_command_torques::PANDA";
