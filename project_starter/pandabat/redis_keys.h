836887b79ffe0132917ec3b7f98a857ecd166d0f
Ryan <rdtnguyen02@LAPTOP-30G2E73K>
2024-05-16 16:41:26 -0700

    panda bat code

---
+++ project_starter/pandabat/redis_keys.h
@@ -0,0 +1,16 @@
+/**
+ * @file redis_keys.h
+ * @brief Contains all redis keys for simulation and control.
+ * 
+ */
+
+const std::string JOINT_ANGLES_KEY = "sai2::sim::panda::sensors::q";
+const std::string JOINT_VELOCITIES_KEY = "sai2::sim::panda::sensors::dq";
+const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::sim::panda::actuators::fgc";
+const std::string CONTROLLER_RUNNING_KEY = "sai2::sim::panda::controller";
+
+const std::string GRIPPER_JOINT_ANGLES_KEY = "sai2::sim::panda_gripper::sensors::q";
+const std::string GRIPPER_JOINT_VELOCITIES_KEY = "sai2::sim::panda_gripper::sensors::dq";
+
+const std::string RIGID_BODY_POS = "sai2::optitrack::rigid_body_pos::1";
+const std::string RIGID_BODY_ORI = "sai2::optitrack::rigid_body_ori::1";
