4109577b129d9ab74a87d059ad114d79b74589ae
ljjh20 <lukehcode@gmail.com>
2025-04-24 21:09:32 -0700

    completed code and plotter for homework 2

--- homework/hw2/hw2.cpp
+++ homework/hw2/hw2.cpp
@@ -1,6 +1,6 @@
 // some standard library includes
 #include <math.h>
-
+#include <fstream>
 #include <iostream>
 #include <mutex>
 #include <string>
@@ -19,7 +19,15 @@
 // for handling ctrl+c and interruptions properly
 #include <signal.h>
 bool runloop = true;
+
 void sighandler(int) { runloop = false; }
+    // handle break for file writing
+    std::ofstream jointFile;
+    void onSigInt(int) {
+    std::cerr << "Caught SIGINT, closing file and exiting.\n";
+    if (jointFile.is_open()) jointFile.close();
+    std::exit(0);
+    }
 
 // namespaces for compactness of code
 using namespace std;
@@ -115,6 +123,14 @@ int main(int argc, char** argv) {
     const double control_freq = 1000;
     SaiCommon::LoopTimer timer(control_freq);
 
+    jointFile.open("../../homework/hw2/q1_joint_pos.csv");
+    if(!jointFile){
+        std::cerr << "ERROR: could not open joint_positions.csv for writing\n";
+        return 1;
+    }
+    jointFile << "t,x,y,z,q0,q1,q2,q3,q4,q5,q6\n"; // header
+    std::size_t counter = 0;
+
     while (runloop) {
         // wait for next scheduled loop
         timer.waitForNextLoop();
@@ -132,26 +148,154 @@ int main(int argc, char** argv) {
 
         // ---------------------------  question 1 ---------------------------------------
         if(controller_number == 1) {
+            // define Kv, Kp
+            Eigen::Vector<double,7> Kp, Kv;
+            Kp.setZero();  Kv.setZero();
 
-            control_torques.setZero();
+            for(int i = 0; i < 6; ++i) {
+                Kp[i] = 400.0;
+                Kv[i] =  50.0;
+            }
+            Kp[6] = 50.0;
+            double d = 0.1425;
+            Kv[6] = -d; // assuming d = 0 (no damping term in URDF)
+
+            Eigen::Vector<double,7> q_desired = robot_q;
+            q_desired[6] = 0.1; // change desired configuration of joint 6
+
+            Eigen::Matrix<double,7,1> tau_proportional =  Kp.asDiagonal() * (robot_q - q_desired);
+            Eigen::Matrix<double,7,1> tau_derivative =  Kv.asDiagonal() * robot_dq;
+            
+            control_torques = -tau_proportional - tau_derivative + robot->coriolisPlusGravity();
+
+                        // write joint values as comma separated
+            if (++counter % 10 == 0) {
+                jointFile
+                    << time << ','
+                    << robot_q(6) << '\n';
+            }
         }
 
         // ---------------------------  question 2 ---------------------------------------
         else if(controller_number == 2) {
+            VectorXd ee_desired(3);
+            ee_desired << 0.3, 0.1, 0.5; 
 
-            control_torques.setZero();
+            VectorXd x = robot->positionInWorld(link_name, pos_in_link);
+            VectorXd xdot = Jv * robot_dq;
+
+            double Kp = 200;
+            double Kv = 30;
+            Eigen::Vector<double,7> Kvj;
+            Kvj.setZero();
+
+            for(int i = 0; i < 7; ++i) {
+                Kvj[i] =  50.0;
+            }
+            Eigen::Matrix<double,7,1> tau_derivative =  Kvj.asDiagonal() * robot_dq;
+            Eigen::Matrix<double,7,1> tau_derivative_null = -1 * N.transpose() * robot->M() * (Kvj.asDiagonal() * robot_dq);
+
+            VectorXd F = Lambda * (-Kp * (x - ee_desired) - Kv * xdot);
+            control_torques = (Jv.transpose() * F) + tau_derivative_null + robot->jointGravityVector();
+            
+            if (++counter % 10 == 0) {
+                jointFile
+                    << time << ','
+                    << x(0) << ','
+                    << x(1) << ','
+                    << x(2) << ','
+                    << robot_q(0) << ','
+                    << robot_q(1) << ','
+                    << robot_q(2) << ','
+                    << robot_q(3) << ','
+                    << robot_q(4) << ','
+                    << robot_q(5) << ','
+                    << robot_q(6) << '\n';
+            }
+
         }
 
         // ---------------------------  question 3 ---------------------------------------
         else if(controller_number == 3) {
+            VectorXd ee_desired(3);
+            ee_desired << 0.3, 0.1, 0.5; 
 
-            control_torques.setZero();
+            Jv = robot->Jv(link_name, pos_in_link);
+	        Lambda = robot->taskInertiaMatrix(Jv);
+	        J_bar = robot->dynConsistentInverseJacobian(Jv);
+	        N = robot->nullspaceMatrix(Jv);
+
+            VectorXd x = robot->positionInWorld(link_name, pos_in_link);
+            VectorXd xdot = robot->linearVelocity(link_name, pos_in_link);
+            MatrixXd Kvj =  5*MatrixXd::Identity(7, 7);
+
+            double Kp = 200;
+            double Kv = 2*std::sqrt(Kp);
+
+            VectorXd p = J_bar.transpose() * robot->jointGravityVector();
+
+            VectorXd F = Lambda * (-1*(Kp * (x - ee_desired)) - (Kv * xdot)) + p;
+            Eigen::Matrix<double,7,1> tau_derivative_null = -1 * N.transpose() * (robot->M()) * (Kvj * robot_dq);
+
+            control_torques = (Jv.transpose() * F) + tau_derivative_null;
+            if (++counter % 10 == 0) {
+                jointFile
+                    << time << ','
+                    << x(0) << ','
+                    << x(1) << ','
+                    << x(2) << ','
+                    << robot_q(0) << ','
+                    << robot_q(1) << ','
+                    << robot_q(2) << ','
+                    << robot_q(3) << ','
+                    << robot_q(4) << ','
+                    << robot_q(5) << ','
+                    << robot_q(6) << '\n';
+            }
+
         }
 
         // ---------------------------  question 4 ---------------------------------------
         else if(controller_number == 4) {
 
-            control_torques.setZero();
+
+            Jv = robot->Jv(link_name, pos_in_link);
+	        Lambda = robot->taskInertiaMatrix(Jv);
+	        J_bar = robot->dynConsistentInverseJacobian(Jv);
+	        N = robot->nullspaceMatrix(Jv);
+
+            VectorXd x = robot->positionInWorld(link_name, pos_in_link);
+            VectorXd xdot = robot->linearVelocity(link_name, pos_in_link);
+            MatrixXd Kvj =  5*MatrixXd::Identity(7, 7);
+            MatrixXd Kpj =  30*MatrixXd::Identity(7, 7);
+
+            double Kp = 200;
+            double Kv = 2*std::sqrt(Kp);
+
+            VectorXd ee_desired(3);
+            ee_desired << 0.3 + 0.1*sin(M_PI*time), 0.1 + 0.1*cos(M_PI*time), 0.5; 
+
+            VectorXd p = J_bar.transpose() * robot->jointGravityVector();
+            VectorXd F = Lambda * (-1*(Kp * (x - ee_desired)) - (Kv * xdot)) + p;
+            Eigen::Matrix<double,7,1> tau_derivative_null = -1* N.transpose() * (robot->M()) * (Kvj * robot_dq + Kpj * robot_q);
+            control_torques = (Jv.transpose() * F) + tau_derivative_null;
+
+            
+            if (++counter % 10 == 0) {
+                jointFile
+                    << time << ','
+                    << x(0) << ','
+                    << x(1) << ','
+                    << x(2) << ','
+                    << robot_q(0) << ','
+                    << robot_q(1) << ','
+                    << robot_q(2) << ','
+                    << robot_q(3) << ','
+                    << robot_q(4) << ','
+                    << robot_q(5) << ','
+                    << robot_q(6) << '\n';
+            }
+
         }
 
         // **********************
@@ -161,6 +305,8 @@ int main(int argc, char** argv) {
         // send to redis
         redis_client.sendAllFromGroup();
     }
+
+    jointFile.close();
 
     control_torques.setZero();
     gravity_comp_enabled = true;
--- python_scripts/trajectory_plotter.py
+++ python_scripts/trajectory_plotter.py
@@ -7,7 +7,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 print("Script directory:", script_dir)
 
 # 2. Build the absolute path to your CSV
-csv_path = os.path.join(script_dir, '..', 'homework', 'hw1', 'q1_joint_pos.csv')
+csv_path = os.path.join(script_dir, '..', 'homework', 'hw2', 'q1_joint_pos.csv')
 csv_path = os.path.normpath(csv_path)   # clean up any “..” or extra slashes
 print("Looking for CSV at:", csv_path)
 
@@ -20,13 +20,28 @@ df = pd.read_csv(csv_path)
 
 plt.figure()
 plt.plot(df['t'], df['q0'], label='Joint 1')
+plt.plot(df['t'], df['q1'], label='Joint 2')
 plt.plot(df['t'], df['q2'], label='Joint 3')
 plt.plot(df['t'], df['q3'], label='Joint 4')
-plt.xlabel('10 * Time (s)')
-plt.ylabel('Joint Angle')
-plt.title('bonus: 2.5 EE payload compensation')
+plt.plot(df['t'], df['q4'], label='Joint 5')
+plt.plot(df['t'], df['q5'], label='Joint 6')
+plt.plot(df['t'], df['q6'], label='Joint 7')
+plt.xlabel('Time (s)')
+plt.ylabel('Joint Angles')
+plt.title('Joint trajectories with joint and task gravity comp')
 
 plt.grid(True)
 plt.legend()
 plt.show()
 
+plt.figure()
+plt.plot(df['t'], df['x'], label='x')
+plt.plot(df['t'], df['y'], label='y')
+plt.plot(df['t'], df['z'], label='z')
+plt.xlabel('Time (s)')
+plt.ylabel('Position')
+plt.title('End effector position')
+
+plt.grid(True)
+plt.legend()
+plt.show()
