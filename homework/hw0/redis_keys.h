6a39e5e6e4ecf59beecb64d5bcc087767e26afa3
enzo-andreacchio <enzo.andreacchio.perso@gmail.com>
2026-03-31 20:25:16 -0700

    Added general setup, HW0 and the corresponding urdf model

---
+++ homework/hw0/redis_keys.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// - read:
+const std::string JOINT_ANGLES_KEY = "sai::sensors::RPRBot::joint_positions";
+const std::string JOINT_VELOCITIES_KEY = "sai::sensors::RPRBot::joint_velocities";
