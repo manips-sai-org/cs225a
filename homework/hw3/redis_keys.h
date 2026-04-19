b58f15f98152072e267e9cdbfbe9c79dd8f80bd6
enzo-andreacchio <enzo.andreacchio.perso@gmail.com>
2026-04-19 11:14:17 -0700

    added hw3

---
+++ homework/hw3/redis_keys.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// - read:
+const std::string JOINT_ANGLES_KEY = "sai::sensors::PANDA::joint_positions";
+const std::string JOINT_VELOCITIES_KEY = "sai::sensors::PANDA::joint_velocities";
+// - write
+const std::string JOINT_TORQUES_COMMANDED_KEY = "sai::commands::PANDA::control_torques";
+const std::string GRAVITY_COMP_ENABLED_KEY = "sai::simviz::gravity_comp_enabled";
